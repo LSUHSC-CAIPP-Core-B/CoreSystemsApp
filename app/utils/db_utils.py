328b1f39142d0c3381add12fe11ea01c55199541
rwiggins1 <wigginsricky94@gmail.com>
2024-05-14 13:04:55 -0500

    added json_Reader util method

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -1,2 +1,11 @@
-class db_connections:
-    pass
+import json
+class db_utils:
+    @staticmethod
+    def json_Reader(path: str, mode: str) -> any:
+        with open(path, mode) as file:
+            config_data = json.load(file)
+        db_config = config_data.get('db_config')
+        db_config
+        db_config = config_data.get('db_config', {})
+
+        return db_config

e0e5536f734fa95fcd464827a9495a64c48996a0
rwiggins1 <wigginsricky94@gmail.com>
2024-05-14 13:27:37 -0500

    Integrated the toDataframe method into util

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -1,3 +1,7 @@
+import mysql.connector as connection
+from datetime import datetime
+import pandas as pd
+import pymysql
 import json
 class db_utils:
     @staticmethod
@@ -12,3 +16,26 @@ class db_utils:
         db_config = config_data.get('db_config', {})
 
         return db_config
+    
+    @staticmethod
+    def toDataframe(query: str, params=None) -> any:
+        """
+        Takes in query, database, and parameter and converts query to a dataframe.
+        
+        query(str): query to convert to dataframe
+        database_name(str): database name for connection
+        param(str)or(None): Parameter to put in query
+
+        return: dataframe from the query passed
+        """
+
+        try:
+            mydb = pymysql.connect(**db_utils.json_Reader('app/Credentials/CoreC.json', 'r'))
+            # Using bind parameters to prevent SQL injection
+            result_dataFrame = pd.read_sql_query(query, mydb, params=params)
+            
+            mydb.close()  # closes the connection
+            return result_dataFrame
+        except Exception as e:
+            print(str(e))
+            mydb.close()

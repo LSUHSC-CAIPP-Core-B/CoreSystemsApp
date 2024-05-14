cf425c3a21b532f92cb8ed3173b1bc6a8c5e6d07
rwiggins1 <wigginsricky94@gmail.com>
2024-05-14 15:24:33 -0500

    Added Date Validators to db_utils

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 import pymysql
 import json
+import re
 class db_utils:
     @staticmethod
     def json_Reader(path: str, mode: str) -> any:
@@ -27,11 +28,34 @@ class db_utils:
 
         try:
             mydb = pymysql.connect(**db_utils.json_Reader(path, 'r'))
-            # Using bind parameters to prevent SQL injection
             result_dataFrame = pd.read_sql_query(query, mydb, params=params)
             
-            mydb.close()  # closes the connection
+            mydb.close()
             return result_dataFrame
         except Exception as e:
             print(str(e))
             mydb.close()
+    
+    def isValidDateFormat(expiration_date: str) -> bool:
+        '''
+        Takes in expiration date to check if valid format for mysql.
+        datePattern defines regex pattern for "YYYY-MM-DD"
+        '''
+
+        datePattern = r"^\d{4}-\d{2}-\d{2}$"
+        
+        # Checks if the string matches the pattern
+        if re.match(datePattern, expiration_date):
+            return True
+        else: # The string does not match the "YYYY-MM-DD" format
+            return False
+
+    def isValidDate(expiration_date: str) -> bool:
+        '''
+        Takes in expiration date to check if valid date for mysql.
+        '''
+        try: # Tries to convert the string to a datetime object
+            datetime.strptime(expiration_date, "%Y-%m-%d")
+            return True
+        except ValueError: # The string is in the correct format but not a valid date
+            return False

5c01e4311175d3b109ffa05668ea7748a8911652
rwiggins1 <wigginsricky94@gmail.com>
2024-05-17 14:38:03 -0500

    Integrated search utils into antibodies routes.py

--- app/CoreC/antibodies/routes.py
+++ app/CoreC/antibodies/routes.py
@@ -14,6 +14,7 @@ from datetime import datetime
 from flask_caching import Cache
 
 from app.utils.db_utils import db_utils
+from app.utils.search_utils import search_utils
 
 app = Flask(__name__)
 cache1 = Cache(app, config={'CACHE_TYPE': 'simple'}) # Memory-based cache
@@ -102,26 +103,7 @@ def create_or_filter_dataframe():
     if len(Uinputs) != 0:
         columns_to_check = ["Company_name", "Target_Name", "Target_Species"]
 
-        threshold = 70  # Threshold for a match
-
-        matches_per_input = [set() for _ in Uinputs]  # List of sets, one for each input
-
-        for input_index, i in enumerate(Uinputs):
-            for index, row in SqlData.iterrows():
-                for column in columns_to_check:
-                    if fuzz.ratio(i, row[column]) > threshold:
-                        matches_per_input[input_index].add(index)  # Adds row index to the set for this input
-                        break  # No need to check other columns for this input
-
-        # Finds the intersection of all sets to ensure each input has at least one matching column in the row
-        all_matches = set.intersection(*matches_per_input) if matches_per_input else set()
-        
-        # renaming columns and setting data variable
-        SqlData.rename(columns={'Box_Name': 'Box Name', 'Company_name': 'Company', 'Catalog_Num': 'Catalog number', 'Target_Name': 'Target', 'Target_Species': 'Target Species', 'Clone_Name': 'Clone', 'Expiration_Date': 'Expiration Date', 'Cost': 'Cost ($)'}, inplace=True)
-        # Gets the filtered dataframe
-        filtered_df = SqlData.loc[list(all_matches)]
-        # Converts to a list of dictionaries
-        data = filtered_df.to_dict(orient='records')
+        data = search_utils.search_data(Uinputs, columns_to_check, 70, SqlData)
         
         # If no match is found displays empty row
         if not data:

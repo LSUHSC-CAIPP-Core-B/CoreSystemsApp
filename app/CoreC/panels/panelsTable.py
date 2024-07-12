c5531487046ed3bf6554cad5e7632d797f2b1167
rwiggins1 <wigginsricky94@gmail.com>
2024-07-12 16:40:24 -0500

    Changed pagination for predefined antibody panels to accept 20 rows per page. Added concrete classes for panels.

---
+++ app/CoreC/panels/panelsTable.py
@@ -0,0 +1,23 @@
+from app.abstract_classes.BaseDatabaseTable import BaseDatabaseTable
+
+
+class PanelsTable(BaseDatabaseTable):
+    """Concrete class
+    
+    Inherits from abstract class BaseDatabaseTable
+
+    :param BaseDatabaseTable: _description_
+    :type BaseDatabaseTable: _type_
+    """
+
+    def display(self, Uinputs: str, sort: str) -> dict:
+        return super().display(Uinputs, sort)
+    
+    def add(self, params: dict) -> None:
+        return super().add(params)
+    
+    def change(self, params: dict) -> None:
+        return super().change(params)
+    
+    def delete(self, primary_key) -> None:
+        return super().delete(primary_key)

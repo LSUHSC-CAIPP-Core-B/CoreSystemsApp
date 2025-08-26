9e71961d910dca76a3f543dcdd537b1926c1c6cc
rwiggins1 <wigginsricky94@gmail.com>
2025-08-26 13:20:07 -0500

    convert data to list of dictionary after filtering is completed

--- app/CoreB/pi_list/routes.py
+++ app/CoreB/pi_list/routes.py
@@ -91,6 +91,7 @@ def pilist():
             cache1.delete('cached_dataframe')
 
         data = PI_table.display(Uinputs, filters['sort'])
+        data = data.to_dict(orient='records')
 
         with app.app_context():
             cache1.set('cached_dataframe', data, timeout=3600)  # Cache for 1 hour (3600 seconds)

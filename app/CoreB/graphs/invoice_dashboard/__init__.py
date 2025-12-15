6cb69c75eef000df4675da98c833659fc1af40ec
rwiggins1 <wigginsricky94@gmail.com>
2025-12-15 15:43:56 -0600

    refactor: remove unused imports

--- app/CoreB/graphs/invoice_dashboard/__init__.py
+++ app/CoreB/graphs/invoice_dashboard/__init__.py
@@ -1,5 +1,3 @@
 from flask import Blueprint
 
 bp = Blueprint('invoice_dashboard', __name__)
-
-from app.CoreB.graphs.invoice_dashboard import routes
--- app/CoreB/graphs/orders_dashboard/__init__.py
+++ app/CoreB/graphs/orders_dashboard/__init__.py
@@ -1,5 +1,3 @@
 from flask import Blueprint
 
 bp = Blueprint('orders_dashboard', __name__)
-
-from app.CoreB.graphs.orders_dashboard import routes
